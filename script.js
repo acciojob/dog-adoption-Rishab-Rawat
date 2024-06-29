119a8f85e2246019ffa33862f8875a33afa7a206
Rishab-Rawat <rawatrishab66@gmail.com>
2024-06-29 10:17:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+cy.visit(baseUrl + "/main.html");
+cy.get("img").eq(0).invoke("attr", "src").then(src => {
+    expect(src).to.equal("golden_retriever_img_link"); // Replace with the actual URL
+});
+
+// Test for the Poodle image
+cy.visit(baseUrl + "/main.html");
+cy.get("img").eq(1).invoke("attr", "src").then(src => {
+    expect(src).to.equal("poodle_img_link"); // Replace with the actual URL
+});
